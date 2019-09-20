02fac70c40bb655f7498f7a4e750b92bc8317440
Mark Stone <markwstone85@gmail.com>
2019-09-20 03:53:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-   array.map! {|i| i.capitalize + !}
+   array.map! do |i| #{i.capitalize + !}
+ end
 end
 
 def long_planeteer_calls# code an argument here
